c06ca3130e059f649632e73508d38f819e114b4e
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2016-01-03 23:18:20 -0500

    add router and routes for storepicker, store, and 404 page

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
 // App
 
 var App = React.createClass({
@@ -9,7 +15,7 @@ var App = React.createClass({
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header/>
+          <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
         <Inventory/>
@@ -24,7 +30,15 @@ var App = React.createClass({
 var Header = React.createClass({
   render : function() {
     return (
-      <p>Header</p>
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
     )
   }
 });
@@ -51,6 +65,16 @@ var Inventory = React.createClass({
   }
 });
 
+// Not Found
+
+var NotFound = React.createClass({
+  render : function() {
+    return (
+      <h1>Not Found!</h1>
+    )
+  }
+});
+
 // StorePicker
 // This will let us make <StorePicker/>
 
@@ -67,4 +91,15 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<App/>, document.getElementById('main'));
+
+// Routes
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+)
+
+ReactDOM.render(routes, document.getElementById('main'));
